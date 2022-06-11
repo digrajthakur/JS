40032c46a50024cf594e56faa98fedfb0e372071
Deepanshu Thakur <86187639+digrajthakur@users.noreply.github.com>
2022-06-11 18:03:16 +0530

    Js Small Project

---
+++ app.js
@@ -0,0 +1,14 @@
+const button = document.querySelector("button")
+const body = document.querySelector("body")
+const color = ['red', 'green', 'blue', 'brown', 'pink', 'violet']
+
+body.style.backgroundColor = 'orange'
+
+
+button.addEventListener('click', changeB)
+
+function changeB() {
+    const colorIndex = parseInt(Math.random() * color.length)
+    body.style.backgroundColor = color[colorIndex]
+
+}
